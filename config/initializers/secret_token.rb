d20dbafc7649b9072805c3eede2291a59a2b59ca
Aishwarya <aishwarya923@gmail.com>
2014-05-26 10:49:53 +0530

    Devise authentication.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TechUp::Application.config.secret_key_base = 'e9735967f2fbcab1a58a2fac4614230ea59c5903ec833bfbf7468b447c42669455fe1eb560c7a017855a1826c7be87113ff0124ee0faf17673570ac3bf64b1dc'
---
+++ db/migrate/20140523094351_fix_user_name_in_user.rb
@@ -0,0 +1,5 @@
+class FixUserNameInUser < ActiveRecord::Migration
+  def change
+  	rename_column :users, :user_name, :username
+  end
+end
